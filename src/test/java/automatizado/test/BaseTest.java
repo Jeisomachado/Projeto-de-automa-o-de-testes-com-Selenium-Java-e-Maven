88654576a3af8fadcfa6744b5b02b9593b5d82cf
Jeison Machado <jeisonrodrigo@hotmail.com>
2026-02-17 09:15:03 -0300

    fix: ativa chrome headless corretamente para rodar na CI Linux

--- src/test/java/automatizado/test/BaseTest.java
+++ src/test/java/automatizado/test/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.io.File;
@@ -16,12 +17,23 @@ public abstract class BaseTest {
     public void deve_iniciar() {
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
 
-        // Caminho dinâmico do projeto (funciona Windows e Linux)
-        String caminho = System.getProperty("user.dir") + File.separator + 
-                         "sistema" + File.separator + "login.html";
+        ChromeOptions options = new ChromeOptions();
+
+        // Se estiver rodando na CI (GitHub Actions)
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
+
+        // Caminho dinâmico
+        String caminho = System.getProperty("user.dir")
+                + File.separator + "sistema"
+                + File.separator + "login.html";
 
         driver.get("file:///" + caminho);
     }
